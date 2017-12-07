ff081ef329d5b9d5f65df4fa4d4d54d26d7fc708
Shivam Dwivedi <shivamd20@gmail.com>
2017-12-07 15:12:59 +0530

    happy birthday working

--- main.js
+++ main.js
@@ -7,19 +7,20 @@ var count = 0;
 	input.innerHTML = msg;
 	input.dispatchEvent(new Event('input', {bubbles: true}));
 	var button = document.querySelector('button.compose-btn-send');
-    button.click();}
+    button.click();
+}
 
 
 
     function wishOnTime(hour,min,msg){
-
    window.setInterval(function(){ // Set interval for checking
     var date = new Date(); // Create a Date object to find out what time it is
     if(date.getHours() === hour && date.getMinutes() === min){ // Check the time
-       
-
         sendMessage(msg);
+    }
+    else{
 
+        alert(date.getHours()+"r"+date.getMinutes());
 
     }
 }, 60000);
