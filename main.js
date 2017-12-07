7aaf0f574ce9723e3122e36b57794851d3fcf101
Shivam Dwivedi <shivamd20@gmail.com>
2017-12-07 13:43:08 +0530

    wrapping everything in a function

--- main.js
+++
@@ -1,7 +0,0 @@
-var text=document.getElementsByClassName('pluggable-input-body copyable-text selectable-text')[0];
-
-
-text.innerText="ramu";
-var btn=document.getElementsByClassName('compose-btn-send')[0];
-
-btn.click();
